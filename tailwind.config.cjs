7aa95b6e1b78015bb1dce9ec4e73cdf12a47b449
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-21 11:31:01 +0700

    chore: add custom color and screen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1080px",
+    },
+    colors: {
+      primary: "#F2F2F2",
+      secondary: "#45577B",
+    },
+    fontFamily: {
+      title: ["Outfit", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
