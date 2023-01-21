90441a6aec6bd90e8882eb8f22957640131f197c
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-21 13:06:43 +0700

    feat: add cross icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -59,22 +59,41 @@ export function Nav() {
               })}
             </ul>
           </div>
-          <button onClick={() => setOpen(!open)} className="md:hidden z-20">
-            <svg
-              viewBox="0 0 24 24"
-              width="30"
-              height="30"
-              stroke="#45577B"
-              stroke-width="2.5"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <line x1="3" y1="12" x2="21" y2="12"></line>
-              <line x1="3" y1="6" x2="21" y2="6"></line>
-              <line x1="3" y1="18" x2="21" y2="18"></line>
-            </svg>
-          </button>
+
+          {open ? (
+            <button onClick={() => setOpen(!open)} className="md:hidden z-20">
+              <svg
+                viewBox="0 0 24 24"
+                width="30"
+                height="30"
+                stroke="#45577B"
+                stroke-width="2.5"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+              </svg>
+            </button>
+          ) : (
+            <button onClick={() => setOpen(!open)} className="md:hidden z-20">
+              <svg
+                viewBox="0 0 24 24"
+                width="30"
+                height="30"
+                stroke="#45577B"
+                stroke-width="2.5"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="3" y1="12" x2="21" y2="12"></line>
+                <line x1="3" y1="6" x2="21" y2="6"></line>
+                <line x1="3" y1="18" x2="21" y2="18"></line>
+              </svg>
+            </button>
+          )}
         </div>
       </Container>
     </nav>
