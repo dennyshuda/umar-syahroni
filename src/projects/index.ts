913e9f4c3e2cd119ce713f070bade5e0ce458359
Denny Setyawan Huda <dennshgit@gmail.com>
2023-01-21 18:37:14 +0700

    feat: add list project

---
+++ src/projects/index.ts
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    id: 1,
+    tag: "Design",
+    title: "Mobile App Landing Design & Service",
+    imgUrl: "./example.png",
+  },
+  {
+    id: 2,
+    tag: "Design",
+    title: "Web Development",
+    imgUrl: "./example.png",
+  },
+  {
+    id: 3,
+    tag: "Design",
+    title: "Web Development",
+    imgUrl: "./example.png",
+  },
+  {
+    id: 4,
+    tag: "Design",
+    title: "Mobile App Landing Design & Service",
+    imgUrl: "./example.png",
+  },
+  {
+    id: 5,
+    tag: "Design",
+    title: "Web Development",
+    imgUrl: "./example.png",
+  },
+  {
+    id: 6,
+    tag: "Design",
+    title: "Web Development",
+    imgUrl: "./example.png",
+  },
+];
